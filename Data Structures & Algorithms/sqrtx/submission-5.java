fd634b483f768adb5d1e2a3a20757b6b54ace043
mike-jira <161240917+mike-jira@users.noreply.github.com>
2026-09-18 19:15:33 +0700

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        int result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            long temp = (long) mid * mid;
+
+            if (temp < x) {
+                result = mid;
+                left = mid + 1;
+            } else if (temp > x) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return result;
+    }
+}
