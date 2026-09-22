934fc5f3e0ae1d91b5d90f3030b3b400989db1ee
mike-jira <161240917+mike-jira@users.noreply.github.com>
2026-09-23 01:11:48 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = left + 1;
+        int maxProfit = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                int profit = prices[right] - prices[left];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            } else {
+                left = right;
+            }
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
